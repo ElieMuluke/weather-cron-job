9942b5aee3fd7b25e34c467f135f1cca49fba65f
Elie Muluke <61221932+ElieMuluke@users.noreply.github.com>
2023-09-07 21:11:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ if __name__ == "__main__":
     if r.status_code == 200:
         data = r.json()
         temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in {city},{country}: {temperature}')
+        logger.info(f'Weather in {city}, {country}: {temperature}')
